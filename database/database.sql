11edcace26c2e230a652ec8fe5afe078e9a38d47
MohamedHanafiAlaoui <hanafimohamed510@gmail.com>
2024-12-09 16:17:44 +0100

    ajouter Table capitale

--- database/database.sql
+++ database/database.sql
@@ -4,6 +4,15 @@ CREATE TABLE Ville
     -> name  VARCHAR(50),
     -> id_pays,
     -> );
+ CREATE TABLE Pays
+    -> (
+    -> id_pays int,
+    -> name VARCHAR(50),
+    -> population int,
+    -> langues VARCHAR(50),
+    -> is_capitale int);
+
+	CREATE TABLE capitale (  is_capitale int,name VARCHAR(50);
 
 
 
