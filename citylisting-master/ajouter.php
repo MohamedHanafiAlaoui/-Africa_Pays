f5daa9b367823cab9b2825a3202f4e44baacd40d
MohamedHanafiAlaoui <hanafimohamed510@gmail.com>
2024-12-13 21:49:52 +0100

    page ajouter link pages

--- citylisting-master/ajouter.php
+++ citylisting-master/ajouter.php
@@ -40,19 +40,24 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
 <nav class="navbar navbar-expand-lg navbar-light bg-Primary">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">AFRICA_PAYS</a>
+    <a class="navbar-brand" href="./index.php">AFRICA_PAYS</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">pays modifer</a>
+          <a class="nav-link active" aria-current="page" href="./ajouter.php">Ajouter pays </a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">city modifer</a>
+          <a class="nav-link" href="./ajouterVille.php">Ajouter Ville</a>
         </li>
-
+        <li class="nav-item">
+          <a class="nav-link" href="./pays.php">Pays </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="./ville.php"> Ville</a>
+        </li>
       </ul>
       <form class="d-flex">
         <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
